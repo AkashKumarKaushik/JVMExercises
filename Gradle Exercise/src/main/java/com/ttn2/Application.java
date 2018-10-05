440fc3e42793bccc3c2be0acb0adb22b41805dbf
Akash <akash.kaushik@tothenew.com>
2018-10-05 23:00:59 +0530

    Gradle Committed

---
+++ Gradle Exercise/src/main/java/com/ttn2/Application.java
@@ -0,0 +1,10 @@
+package com.ttn2;
+
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(Application2.print());
+    }
+
+}
